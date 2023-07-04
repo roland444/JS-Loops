3d4dd73e7b2b3fe1b4083a49776f75306b09cf2b
roland444 <123905024+roland444@users.noreply.github.com>
2023-07-04 20:04:47 +0200

    Added code content for for section

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
-for (let i = 0; i < 10; i++) {
-    console.log(i);
+// for (let i = 0; i < 10; i++) {
+//     console.log(i);
+// }
+
+
+
+// let array = ['a', 'b', 'c', 'd', 'e'];  
+
+// for (let element of array) {
+//     console.log(element);
+// }
+
+
+let object = { a: 1, b: 2, c: 3};
+
+for (let property in object) {
+    console.log(property + ":" +  object[property]);
 }
- 

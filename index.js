24e18c8707f50c2d9a801af6f1996de37c95e696
Krzysztof Roland <krzysztof.roland@wp.pl>
2023-08-18 23:27:39 +0200

    Added forOf section buttons

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 window.onload = () => {
     forSection.init();
+    forOfSection.init();
 }
 
 
@@ -13,14 +14,11 @@ let forSection = {
         
         _forResultBtn.onclick = () => this.showResults();
 
-        _forCancelBtn.onclick = () => {
-            this.cancelResults(this.answersShown);
-        }
+        _forCancelBtn.onclick = () => this.cancelResults(this.answersShown);
     },
     showResults: function() {
         const forResult = document.querySelector("#for-result-section");
-        
-                
+           
         if (!this.answersShown) {
             for (let i = 0; i < 5; i++) { 
                 forResult.insertAdjacentHTML("beforeend" ,`<div class="result-nl" style="color: rgb(66, 182, 66);">${i}</div>`);
@@ -29,14 +27,42 @@ let forSection = {
         }
     },
     cancelResults: function() {
-        console.log(this.answersShown)
         if (this.answersShown) {
             let answers = document.querySelectorAll("#for-result-section > .result-nl");
             answers.forEach((el) => el.remove());
             this.answersShown = false;
         }
     }
-    
-    
+}
+
+let forOfSection = {
+    answersShown: false,
+    init: function() {
+        const _forOfResultBtn = document.getElementById("forOf-result-btn");
+        const _forOfCancelBtn = document.getElementById("forOf-cancel-btn");
+
+        _forOfResultBtn.onclick = () => this.showResults();
+
+        _forOfCancelBtn.onclick = () => this.cancelResult(this.answersShown);
+    },
+    showResults: function() {
+        const forOfResult = document.querySelector("#for-of-result-section");
+
+        if (!this.answersShown) {
+            let array = ["a", "b", "c", "d", "e"];
+            
+            for (let element of array) {
+                forOfResult.insertAdjacentHTML("beforeend", `<div class="result-nl">${element}</div>`)
+            }
+            this.answersShown = true;
+        }
+    },
+    cancelResult: function() {
+        if (this.answersShown) {
+            let answers = document.querySelectorAll("#for-of-result-section > .result-nl");
+            answers.forEach((el) => el.remove());
+            this.answersShown = false;
+        }
+    }
 
 }
